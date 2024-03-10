413c417bc50bd590edeb9597e2448b81c32b13ac
Rubel <rubel.mozumder@outlook.com>
2024-03-10 20:19:40 +0100

    lint.

--- pynxtools_stm/nanonispy/__init__.py
+++ pynxtools_stm/nanonispy/__init__.py
@@ -1,9 +1,1 @@
 from . import read
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-# from . import utils
->>>>>>> 9437866 (_parse_3ds_header now fills leftover header values into header_dict from raw_dict)
-=======
-from . import utils
->>>>>>> 4de304f (replaced test function checking header between two different nanonis header formats)
