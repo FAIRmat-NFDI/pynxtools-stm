3f673bab3ea69f6ff19fc97cc85d9c72f32b2ec9
Rubel <rubel.mozumder@outlook.com>
2024-03-10 20:36:13 +0100

    lint.

--- pynxtools_stm/nanonispy/utils.py
+++ pynxtools_stm/nanonispy/utils.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 import numpy as np
 
+
 def show_grid(arr, sweep_signal):
     """
     Plot 2 out of 3 dimensions of a Grid as an image with a slider to move along third dimension
@@ -22,21 +23,27 @@ def show_grid(arr, sweep_signal):
     # starting + min/max values for slider
     default_energy_index = arr.shape[-1] // 2
     energy_min = 0
-    energy_max = arr.shape[-1] -1
+    energy_max = arr.shape[-1] - 1
 
     fig, ax = plt.subplots()
     ax.set_position([0.125, 0.175, 0.80, 0.80])
-    s_ax = fig.add_axes([0.2, 0.10, 0.65, 0.03]) # axis for slider
-    s_energy_ind = Slider(s_ax, 'Energy', energy_min, energy_max, valinit=default_energy_index)
+    s_ax = fig.add_axes([0.2, 0.10, 0.65, 0.03])  # axis for slider
+    s_energy_ind = Slider(
+        s_ax, "Energy", energy_min, energy_max, valinit=default_energy_index
+    )
 
     im = ax.imshow(arr[:, :, int(s_energy_ind.val)])
-    s_energy_ind.valtext.set_text('{:.2f} mV'.format(sweep_signal[int(s_energy_ind.val)] * 1000))
+    s_energy_ind.valtext.set_text(
+        "{:.2f} mV".format(sweep_signal[int(s_energy_ind.val)] * 1000)
+    )
 
     def update(val):
         im.set_data(arr[:, :, int(s_energy_ind.val)])
         im.autoscale()
 
-        s_energy_ind.valtext.set_text('{:.2f} mV'.format(sweep_signal[int(s_energy_ind.val)] * 1000))
+        s_energy_ind.valtext.set_text(
+            "{:.2f} mV".format(sweep_signal[int(s_energy_ind.val)] * 1000)
+        )
 
     s_energy_ind.on_changed(update)
     plt.show()
