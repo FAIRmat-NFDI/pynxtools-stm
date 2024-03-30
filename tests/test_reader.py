450cfb3e48e013c7035a47f09c8815f79621743d
Rubel <rubel.mozumder@outlook.com>
2024-03-30 11:46:33 +0100

    ruff and fix pytest.

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -17,15 +17,18 @@ from pynxtools.nexus.nxdl_utils import get_nexus_definitions_path
 
 from pynxtools_stm.reader import STMReader
 
-@pytest.mark.parametrize("nxdl,example_data", 
-                         [('NXsts', 'data/sts_nanonis_5e'),
-                          ('NXsts', 'data/sts_nanonis_4_5'),
-                          ('NXsts', 'data/stm_nanonis_5e'),])
-class TestSTMReader:
 
+@pytest.mark.parametrize(
+    "nxdl,example_data",
+    [
+        ("NXsts", "data/sts_nanonis_5e"),
+        ("NXsts", "data/sts_nanonis_4_5"),
+        ("NXsts", "data/stm_nanonis_5e"),
+    ],
+)
+class TestSTMReader:
     # Try with convert()
     def test_example_data(self, nxdl, example_data, tmp_path):
-
         """
         Test the example data for the stm reader
         """
@@ -39,9 +42,7 @@ class TestSTMReader:
         data_dir = os.path.join(os.path.dirname(__file__), example_data)
         input_files = sorted(glob(os.path.join(data_dir, "*")))
         assert nxdl in reader.supported_nxdls
-        nxdl_file = os.path.join(
-            def_dir, "contributed_definitions", f"{nxdl}.nxdl.xml"
-        )
+        nxdl_file = os.path.join(def_dir, "contributed_definitions", f"{nxdl}.nxdl.xml")
 
         root = ET.parse(nxdl_file).getroot()
         template = Template()
