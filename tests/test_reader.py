36835a127889cd4044f12a36f8fb05e2417f35e6
Lukas Pielsticker <50139597+lukaspie@users.noreply.github.com>
2024-05-28 17:05:36 +0200

    use correct reader name

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -32,10 +32,10 @@ module_dir = os.path.dirname(os.path.abspath(__file__))
 @pytest.mark.parametrize(
     "nxdl,reader,files_or_dir",
     [
-        ("NXsts", sts, f"{module_dir}/data/stm_nanonis_5e"),
-        ("NXsts", sts, f"{module_dir}/data/sts_nanonis_5e"),
-        ("NXsts", sts, f"{module_dir}/data/stm_nanonis_4_5"),
-        ("NXsts", sts, f"{module_dir}/data/sts_nanonis_4_5"),
+        ("NXsts", "sts", f"{module_dir}/data/stm_nanonis_5e"),
+        ("NXsts", "sts", f"{module_dir}/data/sts_nanonis_5e"),
+        ("NXsts", "sts", f"{module_dir}/data/stm_nanonis_4_5"),
+        ("NXsts", "sts", f"{module_dir}/data/sts_nanonis_4_5"),
     ],
 )
 def test_stm_reader(nxdl, reader_name, files_or_dir, tmp_path, caplog):
