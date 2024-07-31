9aa9ddb2bd45755592ef85abe19a0c39c6aa88db
Rubel <rubel.mozumder@outlook.com>
2024-07-31 18:17:11 +0200

    Metadata from bruker spm file with .xpm extension.

--- pynxtools_stm/file_parsers/brucker/brukerSPM.py
+++ pynxtools_stm/file_parsers/brucker/brukerSPM.py
@@ -1,4 +1,7 @@
 from pySPM.Bruker import Bruker
+import pySPM
+
+import re
 from typing import Any
 
 
@@ -10,7 +13,7 @@ class BrukerSPM(Bruker):
         # Last part of the administrative path
         last_admin_key_order = ["", 0]
         got_leaf_key = False
-
+        # Includes also all the metadata
         self.parsed_data: dict[str, Any] = {}
         with open(self.path, "rb") as file:
             self.layers = []
@@ -35,9 +38,12 @@ class BrukerSPM(Bruker):
                             self.scanners[-1][args[0]] = args[1:]
                     if line == b"*File list end":
                         break
-
                 # Take care of the all metadata
-                line = line.decode("utf-8")
+                try:
+                    line = line.decode("ascii")
+                except UnicodeDecodeError as e:
+                    line = line.decode("latin1")
+
                 if line.endswith(":"):
                     continue
                 key_val = line.split(": ")
@@ -89,7 +95,132 @@ class BrukerSPM(Bruker):
     def _get_layer_size(self, layer_index, encoding, debug=False):
         return super(BrukerSPM, self)._get_layer_size(layer_index, encoding, debug)
 
-    def get_channel(
+    def get_channel_data(
+        self,
+        channel="Height Sensor",
+        backward=False,
+        corr=None,
+        debug=False,
+        encoding="latin1",
+        mfm=False,
+        mock_data=False,
+    ):
+        """
+        Load the SPM image contained in a channel
+        """
+        for i in range(len(self.layers)):
+            if mfm:
+                # Handle case of MFM, where image data is stored at layer 3 on the backward channel
+                backward = True
+                _type = "Bruker MFM"
+                try:
+                    layer_name = self._get_layer_val(i, "@3:Image Data").decode(
+                        encoding
+                    )
+                except KeyError:
+                    continue
+            else:
+                _type = "Bruker AFM"
+                try:
+                    layer_name = self._get_layer_val(i, "@2:Image Data").decode(
+                        encoding
+                    )
+                except KeyError:
+                    continue
+            result = re.match(r'([^ ]+) \[([^]]*)] "([^"]*)"', layer_name).groups()
+            if result[2] == channel:
+                if debug:
+                    print("channel " + channel + " Found!")
+                bck = False
+                if self._get_layer_val(i, "Line Direction") == b"Retrace":
+                    bck = True
+                if bck == backward:
+                    if debug:
+                        print("Direction found")
+                    var = self._get_layer_val(i, "@2:Z scale").decode(encoding)
+                    if debug:
+                        print("@2:Z scale", var)
+                    if "[" in var:
+                        result = re.match(
+                            r"[A-Z]+\s+\[([^]]+)]\s+\(-?[0-9.]+ .*?\)\s+(-?[0-9.]+)\s+(.*?)$",
+                            var,
+                        ).groups()
+                        if debug:
+                            print(result)
+                        bpp = int(self._get_layer_val(i, "Bytes/pixel"))
+                        if debug:
+                            print("BPP", bpp)
+                        # scale = float(result[1])
+                        scale = float(result[1]) / 256**bpp
+
+                        result2 = self.scanners[0][b"@" + result[0].encode(encoding)][
+                            0
+                        ].split()
+                        if debug:
+                            print("result2", result2)
+                        scale2 = float(result2[1])
+                        zscale = result2[2] if len(result2) > 2 else result2[0]
+                        if b"/V" in zscale:
+                            zscale = zscale.replace(b"/V", b"")
+                        if debug:
+                            print(f"scale: {scale:.3e}")
+                            print(f"scale2: {scale2:.3e}")
+                            print("zscale: " + str(zscale))
+                        var = self._get_layer_val(i, "@2:Z offset").decode(encoding)
+                        result = re.match(
+                            r"[A-Z]+\s+\[[^]]+]\s+\(-?[0-9.]+ .*?\)\s+(-?[0-9.]+)\s+.*?$",
+                            var,
+                        ).groups()
+                        offset = float(result[0])
+                    else:
+                        if debug:
+                            print("mode 2")
+                        result = re.match(
+                            r"[A-Z]+ \(-?[0-9.]+ [^)]+\)\s+(-?[0-9.]+) [\w]+", var
+                        ).groups()
+                        scale = float(result[0]) / 65536.0
+                        scale2 = 1
+                        zscale = b"V"
+                        result = re.match(
+                            r"[A-Z]+ \(-?[0-9.]+ .*?\)\s+(-?[0-9.]+) .*?",
+                            self._get_layer_val(i, "@2:Z offset").decode(encoding),
+                        ).groups()
+                        offset = float(result[0])
+                    if debug:
+                        print("Offset:", offset)
+                    data = (
+                        self._get_raw_layer(i, debug=debug, mock_data=mock_data)
+                        * scale
+                        * scale2
+                    )
+                    xres, yres = self._get_res(i)
+                    if debug:
+                        print("xres/yres", xres, yres)
+                    scan_size = self._get_layer_val(i, "Scan Size").split()
+                    aspect_ratio = [
+                        float(x)
+                        for x in self._get_layer_val(i, "Aspect Ratio").split(b":")
+                    ]
+                    if debug:
+                        print("aspect ratio", aspect_ratio)
+                        print("scan size", scan_size)
+                    if scan_size[2][0] == 126:
+                        scan_size[2] = b"u" + scan_size[2][1:]
+                    size = {
+                        "x": float(scan_size[0]),
+                        "y": float(scan_size[1]) * yres / xres,
+                        "unit": scan_size[2].decode(encoding),
+                    }
+                    return {
+                        "channel": [channel, "Topography"][channel == "Height Sensor"],
+                        "BIN": data,
+                        "real": size,
+                        "_type": _type,
+                        "zscale": zscale.decode(encoding),
+                        "corr": corr,
+                    }
+
+    def get_channel_image(
         self,
         channel="Height Sensor",
         backward=False,
--- pynxtools_stm/file_parsers/brucker/note.md
+++ pynxtools_stm/file_parsers/brucker/note.md
@@ -3,3 +3,9 @@
 1.   https://pypi.org/project/access2theMatrix/ (not tested yet)
 2. https://github.com/KoenImdea/Matrix-Image-Analysis/tree/main (not tested yet)
    This lib may help to understand what type of data is rendered by python package 1.
+
+3. Got some endocing that have been tested out to the file `sm4` file
+       encodings[] = {
+        "UTF-16", "CP1252", "CP1251", "CP1250", "CP1253", "CP1254", "CP1255", "CP1256", "CP1257", "CP1258",
+    };
+   in line : 622 in file anfatec from the Gwyddion package. Seems cp1255 works though not all the data.
