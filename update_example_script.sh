fc61a49c1a834adbe0b03a494b9018b7d918d672
Rubel <rubel.mozumder@outlook.com>
2024-11-07 16:55:33 +0100

    Update examples after applying of all review comments from LEv

--- update_example_script.sh
+++ update_example_script.sh
@@ -10,17 +10,17 @@ find tests/data/nanonis/sts/version_gen_5e_with_described_nxdata -type f -name '
 
 # AFM Nanonis
 echo " !!! Converting Nanonis AFM data !!! "
-find tests/data/nanonis/afm/version_gen_4_default_config -type f ! -name '*.nxs' | xargs dataconverter --nxdl NXafm --reader spm --output output_afm_4_with_default_config.nxs
-find tests/data/nanonis/afm/version_gen_4_default_config -type f -name '*.nxs' | xargs mv output_afm_4_with_default_config.nxs
+find tests/data/nanonis/afm/version_gen_4_default_config -type f ! -name '*.nxs' | xargs dataconverter --nxdl NXafm --reader spm --output afm_4_with_default_config.nxs
+find tests/data/nanonis/afm/version_gen_4_default_config -type f -name '*.nxs' | xargs mv afm_4_with_default_config.nxs
 
-find tests/data/nanonis/afm/version_gen_4_with_described_nxdata -type f ! -name '*.nxs' | xargs dataconverter --nxdl NXafm --reader spm --output output_afm_4_with_described_nxdata.nxs
-find tests/data/nanonis/afm/version_gen_4_with_described_nxdata -type f -name '*.nxs' | xargs mv output_afm_4_with_described_nxdata.nxs
+find tests/data/nanonis/afm/version_gen_4_with_described_nxdata -type f ! -name '*.nxs' | xargs dataconverter --nxdl NXafm --reader spm --output afm_4_with_described_nxdata.nxs
+find tests/data/nanonis/afm/version_gen_4_with_described_nxdata -type f -name '*.nxs' | xargs mv afm_4_with_described_nxdata.nxs
 
 # STM Nanonis
 echo " !!! Converting Nanonis STM data !!! "
-find tests/data/nanonis/stm/version_gen_4_5_default_config -type f ! -name '*.nxs' | xargs dataconverter --nxdl NXstm --reader spm --output output_stm_4_5_default_config.nxs
-find tests/data/nanonis/stm/version_gen_4_5_default_config -type f -name '*.nxs' | xargs mv output_stm_4_5_default_config.nxs
+find tests/data/nanonis/stm/version_gen_4_5_default_config -type f ! -name '*.nxs' | xargs dataconverter --nxdl NXstm --reader spm --output stm_4_5_default_config.nxs
+find tests/data/nanonis/stm/version_gen_4_5_default_config -type f -name '*.nxs' | xargs mv stm_4_5_default_config.nxs
 
-find tests/data/nanonis/stm/version_gen_4_5_with_described_nxdata -type f ! -name '*.nxs' | xargs dataconverter --nxdl NXstm --reader spm --output output_stm_4_5_with_described_nxdata.nxs
-find tests/data/nanonis/stm/version_gen_4_5_with_described_nxdata -type f -name '*.nxs' | xargs mv output_stm_4_5_with_described_nxdata.nxs
+find tests/data/nanonis/stm/version_gen_4_5_with_described_nxdata -type f ! -name '*.nxs' | xargs dataconverter --nxdl NXstm --reader spm --output stm_4_5_with_described_nxdata.nxs
+find tests/data/nanonis/stm/version_gen_4_5_with_described_nxdata -type f -name '*.nxs' | xargs mv stm_4_5_with_described_nxdata.nxs
 
